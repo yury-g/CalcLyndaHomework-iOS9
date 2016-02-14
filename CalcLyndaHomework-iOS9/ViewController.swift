5ed12c5a4aea5ba3f404baec5df9e3dcddb72142
Voltaire's Ghost <yury@pulsesensor.com>
2016-02-14 15:43:38 -0500

    Adding Life To Functions

--- CalcLyndaHomework-iOS9/ViewController.swift
+++ CalcLyndaHomework-iOS9/ViewController.swift
@@ -8,7 +8,52 @@
 
 import UIKit
 
+enum modes{
+    case NOT_SET
+    case ADD
+    case SUB
+}
+
+
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    var labelString: String = "0"
+    var currentMode: modes = modes.NOT_SET
+    var savedNum: Int = 0
+    var lastButtonWasMode: Bool = false
+    
+    @IBAction func tappedNumber(sender: UIButton) {
+          let theTappedNum = Int(sender.titleLabel!.text!)
+        
+        savedNum = theTappedNum!
+        print("savedNum=\(savedNum)")
+        
+        }
+    
+    
+    @IBAction func tappedPlus(sender: UIButton) {
+    }
+    @IBAction func tappedMinus(sender: UIButton) {
+    }
+    @IBAction func tappedEQ(sender: AnyObject) {
+    }
+    
+    @IBAction func tappedCleared(sender: UIButton) {
+    }
+    func tappedNumberUser
+(num: Int){
+        
+    }
+    
+    func updateText(){
+        labelString = String("\(savedNum)")
+        label.text = labelString
+    }
+    
+    func changeMode(newMode: modes){
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
