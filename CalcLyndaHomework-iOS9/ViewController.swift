8b532c7f9ac942be1ce746e109f22e1cc7e5c5ec
Voltaire's Ghost <yury@pulsesensor.com>
2016-02-14 14:09:36 -0500

    Initial Commit

---
+++ CalcLyndaHomework-iOS9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalcLyndaHomework-iOS9
+//
+//  Created by yodyodsmall on 2/14/16.
+//  Copyright © 2016 yuryg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
