4391116645c4794fbbc616e0e9c989206417b838
samuraipapa <yury.gitman@gmail.com>
2016-02-15 16:03:05 -0500

    updates voice and lang

--- CalcLyndaHomework-iOS9/ViewController.swift
+++ CalcLyndaHomework-iOS9/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 enum modes{
     case NOT_SET
@@ -16,7 +18,10 @@ enum modes{
 }
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
+    
+    // MARK: Calc Function Related
+    // Calc Related  Based on Lynda.com Programming for Non-Programmers: iOS 9.  Thanks!
     
     // Default State Of Calc
     var labelString: String = "0"
@@ -25,6 +30,21 @@ class ViewController: UIViewController {
     var savedNum: Int = 0
     var totalNum: Int = 0
     var lastButtonWasMode: Bool = false
+    
+    
+    
+    // MARK: - ViewController Class-Wide Variables
+    // MARK: Speach Related
+    let mySpeechSynth = AVSpeechSynthesizer()
+    var myRate: Float = 0.50
+    var myPitch: Float = 1.15
+    var myVolume: Float = 0.92
+    var currentLang = ("en-US", "English","United States","American English ","🇺🇸")
+    
+
+    
+    
+    
    
     @IBOutlet weak var label: UILabel!
     
@@ -167,5 +187,98 @@ class ViewController: UIViewController {
     }
 
 
+    
+    //MARK:   UIPickerView Outlets
+    @IBOutlet weak var myLangPicker: UIPickerView!
+    
+    //MARK: - Speaking Machine
+    
+    func speakThisString(passedString: String){
+        
+        mySpeechSynth.stopSpeakingAtBoundary(AVSpeechBoundary.Immediate)
+        
+        let myUtterance = AVSpeechUtterance(string: passedString)
+        myUtterance.rate = myRate
+        myUtterance.pitchMultiplier = myPitch
+        myUtterance.volume = myVolume
+        myUtterance.voice = AVSpeechSynthesisVoice(language: currentLang.0)
+        mySpeechSynth.speakUtterance(myUtterance)
+        
+        
+        
+    }
+
+    
+    //MARK: - Langugage UIPickerView Methods
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return langCodeAll38.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        
+        pickerView.showsSelectionIndicator = true
+        
+        let myString = "\(langCodeAll38[row].4) \(langCodeAll38[row].3)"
+        
+        return myString
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        currentLang = langCodeAll38[row]
+        speakThisString(currentLang.3)
+    }
+    
+
+    
+    
+    //MARK: - Data Model
+    
+    // current lang array has known typos, to fix in future.
+    var langCodeAll38 = [
+        ("en-US",  "English", "United States", "American English","🇺🇸"),
+        ("ar-SA","Arabic","Saudi Arabia","العربية","🇸🇦"),
+        ("cs-CZ", "Czech", "Czech Republic","český","🇨🇿"),
+        ("da-DK", "Danish","Denmark","Dansk","🇩🇰"),
+        ("de-DE",       "German", "Germany", "Deutsche","🇩🇪"),
+        ("el-GR",      "Modern Greek",        "Greece","ελληνική","🇬🇷"),
+        ("en-AU",     "English",     "Australia","Aussie","🇦🇺"),
+        ("en-GB",     "English",     "United Kingdom", "Queen's English","🇬🇧"),
+        ("en-IE",      "English",     "Ireland", "Gaeilge","🇮🇪"),
+        ("en-ZA",       "English",     "South Africa", "South African English","🇿🇦"),
+        ("es-ES",       "Spanish",     "Spain", "Español","🇪🇸"),
+        ("es-MX",       "Spanish",     "Mexico", "Español de México","🇲🇽"),
+        ("fi-FI",       "Finnish",     "Finland","Suomi","🇫🇮"),
+        ("fr-CA",       "French",      "Canada","Français du Canada","🇨🇦" ),
+        ("fr-FR",       "French",      "France", "Français","🇫🇷"),
+        ("he-IL",       "Hebrew",      "Israel","עברית","🇮🇱"),
+        ("hi-IN",       "Hindi",       "India", "हिन्दी","🇮🇳"),
+        ("hu-HU",       "Hungarian",    "Hungary", "Magyar","🇭🇺"),
+        ("id-ID",       "Indonesian",    "Indonesia","Bahasa Indonesia","🇮🇩"),
+        ("it-IT",       "Italian",     "Italy", "Italiano","🇮🇹"),
+        ("ja-JP",       "Japanese",     "Japan", "日本語","🇯🇵"),
+        ("ko-KR",       "Korean",      "Republic of Korea", "한국어","🇰🇷"),
+        ("nl-BE",       "Dutch",       "Belgium","Nederlandse","🇧🇪"),
+        ("nl-NL",       "Dutch",       "Netherlands", "Nederlands","🇳🇱"),
+        ("no-NO",       "Norwegian",    "Norway", "Norsk","🇳🇴"),
+        ("pl-PL",       "Polish",      "Poland", "Polski","🇵🇱"),
+        ("pt-BR",       "Portuguese",      "Brazil","Portuguese","🇧🇷"),
+        ("pt-PT",       "Portuguese",      "Portugal","Portuguese","🇵🇹"),
+        ("ro-RO",       "Romanian",        "Romania","Română","🇷🇴"),
+        ("ru-RU",       "Russian",     "Russian Federation","русский","🇷🇺"),
+        ("sk-SK",       "Slovak",      "Slovakia", "Slovenčina","🇸🇰"),
+        ("sv-SE",       "Swedish",     "Sweden","Svenska","🇸🇪"),
+        ("th-TH",       "Thai",        "Thailand","ภาษาไทย","🇹🇭"),
+        ("tr-TR",       "Turkish",     "Turkey","Türkçe","🇹🇷"),
+        ("zh-CN",       "Chinese",     "China","漢語/汉语","🇨🇳"),
+        ("zh-HK",       "Chinese",   "Hong Kong","漢語/汉语","🇭🇰"),
+        ("zh-TW",       "Chinese",     "Taiwan","漢語/汉语","🇹🇼")
+    ]
+    
+    
 }
 
